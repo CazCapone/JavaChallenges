e89f15d09d066121510f0bf48fb10cd61974b03a
Joseph Duclos <43616909+CazCapone@users.noreply.github.com>
2019-01-11 14:20:49 -0700

    Dice Done

--- src/javachallenges/DiceRoller.java
+++ src/javachallenges/DiceRoller.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javachallenges;
 
 import java.util.Random;
@@ -11,65 +6,55 @@ import java.util.Scanner;
 /**
  *
  * @author jduclos1
+ * Dungeons and Dragons Dice Roller - for different dice sides
  */
-public class DiceRoller {
-    
+public class DiceRoller {  
     static int diceCount = 0; //between 1 - 100
     static int diceSide = 0; //between 2 - 100
     static int diceTotal = 0;
-    
-    
-    
-    
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args){
-        String input = null;      
-        Scanner scan = new Scanner(System.in);
+        StartGame();
+    }
+    
+    public static void StartGame(){
+        String input = null;   
+        String answer = null;
         System.out.println("Please enter dice count and side number in #d# format. ");
         input = scan.nextLine();
         RollDice(input);
         
-        System.out.println(diceCount);
-        System.out.println(diceSide);
+        System.out.println("Would you like another roll? Y or N ");
+        answer = scan.nextLine();
+        if (answer.equals("Y") || answer.equals("y") || answer.equals("yes") || answer.equals("Yes") || answer.equals("YES")){
+            StartGame();
+        }else{
+            System.out.println("Thanks for playing.");
+        }
     }
-    
-    
-    
-    
-    
-    public static int RollDice(String s){
+
+    public static void RollDice(String s){
+        if (!s.contains("d")){
+            System.out.println("Enter dice count and side number in #d# format! ");
+        }else{
         String[] count = s.split("d");
            
         diceCount = Integer.parseInt(count[0]);
         diceSide = Integer.parseInt(count[1]);   
         
-        if ((diceCount < 1 || diceCount > 100) && (diceSide < 2 || diceSide > 100)){
+        if (diceCount < 1 || diceCount > 100 || diceSide < 2 || diceSide > 100){
             System.out.println("No");
         }else{
-            Random roll = new Random(); 
+            Random rand = new Random(); 
             for (int i = 0; i < diceCount; i++){
-                diceTotal += roll.nextInt(diceSide) + 1; 
+                int roll;
+                roll = rand.nextInt(diceSide) + 1;
+                diceTotal += roll; 
+                System.out.println("You have rolled a " + roll + " on die #" + (i + 1));
             }
-        
-            
-            
         }
-        
-
-        System.out.println("total " + diceTotal);
-        
-        return diceTotal;
-        //return diceTotal;
-    }
-    
-    
-    
-/*5d12
-6d4
-1d2
-1d8
-3d6
-4d20
-100d100
-*/    
-    
+        System.out.println("Your Total Roll Equals " + diceTotal);
+        }
+    } 
 }
